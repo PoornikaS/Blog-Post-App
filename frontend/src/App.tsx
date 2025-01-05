bbfffb3e154f8f74f803febdc5cb24325207568f
PoornikaS <148691734+PoornikaS@users.noreply.github.com>
2025-01-05 16:37:33 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/layout/Navbar';
 import LoginForm from './components/auth/LoginForm';
